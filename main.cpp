9bf352311153788514abe71b1dc5ee83474e29cd
tieway59 <tieway59@foxmail.com>
2019-07-29 22:01:40 +0800

    更新

--- main.cpp
+++ main.cpp
@@ -1,104 +1,105 @@
-#define myDebug(x) cerr<<#x<<" "<<x<<endl
 
-#include <string.h>
-#include <algorithm>
-#include <iostream>
 
+#include<bits/stdc++.h>
 
 using namespace std;
-const int INF = 0x3f3f3f3f;
-const int MAXN = 1e3 + 7;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ll, int> pli;
+#define rep(x, a, b) for(register int x=a;x<=b;x++)
+#define _dbg(x) cout<<x<<'\n';
+#define T(x) int T;scanf("%d",&T);rep(x,1,T)
+
+const int N = 2e5 + 5;
+ll a[N];
+pli ls[N];
+int ans[N], ind[N];
+
+//template <typename _Tp> inline _Tp read(_Tp&x){
+//    char c11=getchar(),ob=0;x=0;
+//    while(c11^'-'&&!isdigit(c11))c11=getchar();if(c11=='-')c11=getchar(),ob=1;
+//    while(isdigit(c11))x=x*10+c11-'0',c11=getchar();if(ob)x=-x;return x;
+//}
 
 
-struct Edge {
-    int u, v, nx; // ,w
-} e[MAXN << 2];
+#define val first
+#define num second
 
-int head[MAXN], cntEd;
+pll tree[N << 2];
 
-inline void addEdge(int u, int v) {
-    e[cntEd] = {u, v, head[u]};
-    head[u] = cntEd++;
+#define lson rt<<1
+#define rson rt<<1|1
+
+void build(int rt, int l, int r) {
+    if (l == r) {
+        tree[rt].num = 0;
+        tree[rt].val = 0;
+        return;
+    }
+    int mid = (l + r) >> 1;
+    build(lson, l, mid);
+    build(rson, mid + 1, r);
+    tree[rt].num = tree[rt].val = 0;
 }
 
-int ind[MAXN], oud[MAXN];
-int Xind[MAXN], Xoud[MAXN];
-int dfn[MAXN], low[MAXN], scc[MAXN], stk[MAXN], index = 0, sccnum = 0, top = 0;
-
-void tarjan(int root) {
-    if (dfn[root]) return;
-    dfn[root] = low[root] = ++index;
-    stk[++top] = root;
-    for (int i = head[root]; ~i; i = e[i].nx) {
-        int v = e[i].v;
-        if (!dfn[v]) {    //如果v结点未访问过
-            tarjan(v);
-            low[root] = min(low[root], low[v]);
-        } else if (!scc[v]) {    //如果还在栈内
-            low[root] = min(low[root], dfn[v]);
-        }
+void update(int rt, int l, int r, int x) {
+    if (l == r) {
+        tree[rt].num++;
+        tree[rt].val += ls[x].first;
+        return;
     }
-    if (low[root] == dfn[root]) {    //后代不能找到更浅的点
-        sccnum++;
-        for (;;) {
-            int x = stk[top--];
-            scc[x] = sccnum;
-            if (x == root) break;
-        }
-    }
+    int mid = (l + r) >> 1;
+    if (x <= mid)update(lson, l, mid, x);
+    else update(rson, mid + 1, r, x);
+    tree[rt].num = tree[lson].num + tree[rson].num;
+    tree[rt].val = tree[lson].val + tree[rson].val;
 }
 
+ll k;
+
+ll query(int rt, int l, int r, int x, ll v) {
+    if (l == r) {
+        ll tp = min(v / ls[l].first, tree[rt].num);
+        k += tp;
+        return tp * ls[l].first;
+    } else if (tree[rt].val <= v) {
+        k += tree[rt].num;
+        return tree[rt].val;
+    }
+    int mid = (l + r) >> 1;
+    ll ret = query(lson, l, mid, x, v);
+    if (ret == tree[lson].val)ret += query(rson, mid + 1, r, x, v - ret);
+    return ret;
+}
 
 int main() {
-    memset(head, -1, sizeof head);
-
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    int n;
-
-    cin >> n;
-    for (int v, i = 1; i <= n; i++) {
-        while (cin >> v && v) {
-            addEdge(i, v);
+    T(ca) {
+        int n;
+        ll m;
+        n = IO.nI();
+        m = IO.nI();
+        rep(i, 1, n) {
+//            read(a[i]);
+            a[i] = IO.nll();
+            ls[i].first = a[i];
+            ls[i].second = i;
         }
+        sort(ls + 1, ls + 1 + n);
+        rep(i, 1, n) {
+            ind[ls[i].second] = i;
+        }
+        build(1, 1, n);
+        rep(i, 1, n) {
+            k = 0;
+            ll tp = query(1, 1, n, ind[i], m - a[i]);
+            printf("%d ", 1ll * i - 1ll - k);
+            update(1, 1, n, ind[i]);
+        }
+        printf("\n");
     }
-    for (int i = 1; i <= n; i++)
-        if (!dfn[i]) tarjan(i);
-
-    int ans1 = 0;
-    int ans2 = 0;
-
-    for (int u, v, i = 0; i < cntEd; i++) {
-        u = scc[e[i].u];
-        v = scc[e[i].v];
-        if (u != v) {
-            ind[v]++;
-            oud[u]++;
-        }
-    }
-    for (int i = 1; i <= sccnum; i++) {
-        if (ind[i] == 0) {
-            ans1++;
-        }
-        if (oud[i] == 0) {
-            ans2++;
-        }
-    }
-    ans2 = max(ans2, ans1);
-
-    if (sccnum == 1)ans1 = 1, ans2 = 0;
-    cout << ans1 << endl << ans2 << endl;
-
     return 0;
 }
 
-/*
 
- 4
- 2 0
- 1 0
- 4 0
- 3 0
 
- */
---
+++ mycode/fast IO int.cpp
@@ -0,0 +1,24 @@
+
+inline void read(int &x) {
+    char ch;
+    bool flag = false;
+    for (ch = getchar(); !isdigit(ch); ch = getchar())if (ch == '-') flag = true;
+    for (x = 0; isdigit(ch); x = x * 10 + ch - '0', ch = getchar());
+    x = flag ? -x : x;
+}
+
+inline void write(int x) {
+    static const int maxlen = 100;
+    static char s[maxlen];
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (!x) {
+        putchar('0');
+        return;
+    }
+    int len = 0;
+    for (; x; x /= 10) s[len++] = x % 10 + '0';
+    for (int i = len - 1; i >= 0; --i) putchar(s[i]);
+}
