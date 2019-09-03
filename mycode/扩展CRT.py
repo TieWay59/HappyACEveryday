2b562f63a6cad525075c922b86a307b63998c24a
tieway59 <tieway59@foxmail.com>
2019-09-03 21:39:01 +0800

    add templates.

---
+++ mycode/扩展CRT.py
@@ -0,0 +1,61 @@
+# https://ac.nowcoder.com/acm/contest/890/D
+# maybe not available.
+
+
+ai = [0]
+bi = [0]
+ 
+ 
+def exgcd(a, b, x, y):
+    if b == 0:
+        x = 1
+        y = 0
+        return a, x, y
+    gcd, x, y = exgcd(b, a % b, x, y)
+    tp = x
+    x = y
+    y = tp - (a // b) * y
+    return gcd, x, y
+ 
+ 
+def excrt():
+    m = bi[1]
+    ans = ai[1]
+    for i in range(2, n + 1):
+        x = 0
+        y = 0
+        aa = m
+        bb = bi[i]
+        c = (ai[i] - ans % bb + bb) % bb
+        gcd, x, y = exgcd(aa, bb, x, y)
+        bg = bb // gcd
+        if c % gcd != 0:
+            return -1
+ 
+        x = x * (c // gcd) % bg
+        ans = ans + x * m
+        m = m * bg
+        ans = (ans % m + m) % m
+    return (ans % m + m) % m
+ 
+ 
+def main():
+    global n
+    n, m = map(int, input().split())
+    # n, m = int(input())
+    for i in range(1, n + 1):
+        ub, ua = map(int, input().split())
+        bi.append(ub)
+        ai.append(ua)
+    ans = excrt()
+    if ans == -1:
+        print("he was definitely lying")
+    else:
+        if ans <= m:
+            print(ans)
+        else:
+            print("he was probably lying")
+ 
+ 
+if __name__ == '__main__':
+    main()
