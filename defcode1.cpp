7901c382b9c30d3e8d71ceeaa36ce76937d0af4c
tieway59 <tieway59@foxmail.com>
2019-09-11 17:31:28 +0800

    + papers

---
+++ defcode1.cpp
@@ -0,0 +1,22 @@
+
+#include <bits/stdc++.h>
+
+#define  _debug(x) cerr<<#x << " = " << x<<endl
+using namespace std;
+
+typedef long long ll;
+const int MOD = -1;
+const int INF = -1;
+const int MAXN = -1;
+
+int kase, Kase;
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << "hello my O friend!"
+         << endl;
+    return 0;
+}
