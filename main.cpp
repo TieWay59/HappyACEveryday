023b492b74cfe8a859487c0d755363bfd0b1480b
tieway59 <tieway59@foxmail.com>
2019-09-16 12:32:21 +0800

    Merge branch 'master' of D:\HappyACEveryday with conflicts.

--- main.cpp
+++ main.cpp
@@ -1,114 +1,56 @@
 /*
-*  https://nanti.jisuanke.com/t/41402
+*  https://www.jisuanke.com/contest/3007
 *
 */
 //#include <bits/stdc++.h>
-#include <iostream>
+#include <stdio.h>
 #include <algorithm>
 #include <vector>
 #include <string.h>
-#include <tuple>
 
 #define  _debug(x) cerr<<#x<<" = "<<x<<endl
 using namespace std;
 typedef long long ll;
-const int MAXN = 1e5 + 59;
-const int MAXM = 2e5 + 59;
-const ll MOD = 998244353;
+const int MAXN = 2e2 + 59; //todo change the size
+
+const int MAXM = 2e6 + 59;
+const ll MOD = 1e9 + 7;
 const ll INF = 0x0f0f0f0f;
 
-int fa[MAXN];
-int sz[MAXN];
-
-int findf(int x) {
-    if (x == fa[x])return x;
-    else {
-        sz[fa[x]] += sz[x];
-        sz[x] = 0;
-        return fa[x] = findf(fa[x]);
-    }
-}
-
-void mergf(int x, int y) {
-    int fx = findf(x);
-    int fy = findf(y);
-    if (fx == fy)return;
-    if (sz[fx] <= sz[fy]) {
-        sz[fx] += sz[fy];
-        sz[fy] = 0;
-        fa[fy] = fx;
-    } else {
-        sz[fy] += sz[fx];
-        sz[fx] = 0;
-        fa[fx] = fy;
-    }
-}
-
-
-int Kase;
-int n, m, k;
-pair<int, int> e[MAXM];
-int a[MAXN];
-bool tag[MAXN];
-bool vis[MAXN];
-vector<int> g[MAXN];
+int kase;
+ll k, n;
+ll a[MAXN];
+ll sum[MAXN];
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(nullptr);
+    scanf("%d", &kase);
+    while (kase--) {
+        scanf("%lld%lld", &k, &n);
 
-    cin >> Kase;
-    while (Kase--) {
-        cin >> n >> m >> k;
-        for (int i = 1; i <= n; ++i) {
-            fa[i] = i;
-            sz[i] = 1;
-            tag[i] = false;
-            vis[i] = false;
-            g[i].clear();
-        }
-        for (int i = 1; i <= m; ++i) {
-            cin >> e[i].first >> e[i].second;
+        for (int i = 1; i <= 2 * k; i++) {
+            scanf("%lld", a + i);
         }
         for (int i = 1; i <= k; ++i) {
-            cin >> a[i];
-            tag[a[i]] = true;
-            sz[a[i]] = 0;
+            sum[i] = (sum[i - 1] + a[i]) % MOD;
         }
-        for (int i = 1; i <= m; ++i) {
-            if (tag[e[i].first] == 0 && tag[e[i].second] == 0) {
-                mergf(e[i].first, e[i].second);
-            }
+        ll ans = 0;
+        if (k <= n) {
+            ans = (sum[k] + a[k + 1] * (n - k) % MOD) % MOD;
+        } else {
+            ans = sum[n];
         }
-        for (int u, v, i = 1; i <= m; ++i) {
-            tie(u, v) = e[i];
-
-            v = findf(v);
-            u = findf(u);
-
-            if (tag[u] || tag[v]) {
-                g[u].emplace_back(v);
-                g[v].emplace_back(u);
-            }
-        }
-
-        int start = findf(1);
-        double ans = sz[start];
-        double tmp = 0;
-
-        for (auto u:g[start]) {
-            if (vis[u])continue;
-            else vis[u] = true;
-
-            double sum = 0;
-            for (auto v:g[u])if (v != start)sum += sz[v];
-            sum /= 1.0 * g[u].size();
-
-            if (sum > tmp) tmp = sum;
-        }
-        ans += tmp;
-        printf("%.6f\n", ans);
+        printf("%lld\n", ans);
     }
 
     return 0;
 }
+/*
+2
+1 9000000000000000
+2 2
+2 8
+6 5 5 5
+
+ */
