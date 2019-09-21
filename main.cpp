a31080b85d3e6d62ece5826c8cba9b1785120a3b
tieway59 <tieway59@foxmail.com>
2019-09-22 00:34:53 +0800

    Merge branch 'master' of D:\HappyACEveryday with conflicts.

--- main.cpp
+++ main.cpp
@@ -1,52 +1,68 @@
-
-
 /*
- * https://codeforces.com/contest/1216/problem/F
- *
- */
-#include <bits/stdc++.h>
+*  https://codeforc.es/contest/1220/problem/D?csrf_token=a6aa4329bb73ab3e1847c9e973e7af85
+*/
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string.h>
+#include <tuple>
 
-#define  _debug(x) cerr<<#x << " = " << x<<endl
+#define  _debug(x) cerr<<#x<<" = "<<x<<endl
 using namespace std;
-
 typedef long long ll;
-const int MOD = -1;
-const double eps = 1e-3;
-const int MAXN = 3e3 + 59;
-const ll INF = 0x3f3f3f3f3f3f3f;
+typedef long long lll;
+const int MAXN = 3e5 + 59;
+const int MAXM = 2e5 + 59;
+const ll MOD = 998244353;
+const ll INF = 2e18 + 59;
+
 
 int kase;
-int n, m, k;
-
-int f[MAXN];
-
-struct node {
-    int a, b;
-} p[MAXN];
+int n;
+ll dp[MAXN][3];
+ll a[MAXN];
+ll b[MAXN];
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    cin >> n >> m >> k;
-    for (int i = 2; i <= 2 + k; ++i) {
-        cin >> f[i];
-        f[i] += f[i - 1];
-    }
-    for (int j = 1; j <= m; ++j) {
-        cin >> p[j].a >> p[j].b;
-    }
-    sort(p + 1, p + 1 + m, [](node x, node y) {
-        return x.b * y.a > y.b * x.a;
-    });
-    int sum = f[n - 2];
-    int ans = sum;
-    for (int i = 1; i <= m; ++i) {
-        if (sum >= p[i].a) {
-            ans += sum / p[i].a * p[i].b;
-            sum %= p[i].a;
+    cin >> kase;
+    while (kase--) {
+        memset(dp, 0, sizeof dp);
+        cin >> n;
+        for (int i = 1; i <= n; ++i) {
+            cin >> a[i] >> b[i];
         }
+
+//        dp[0][0] = dp[0][1] = dp[0][2] = INF;
+        dp[1][1] = b[1];
+        dp[1][2] = b[1] + b[1];
+
+        for (ll i = 2; i <= n; ++i) {
+            dp[i][0] = dp[i][1] = dp[i][2] = INF;
+
+            for (ll j1 = 0; j1 < ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++3; ++j1) {
+                for (ll j2 = 0; j2 < 3; ++j2) {
+
+                    if (a[i - 1] + j2 != a[i] + j1) {
+                        dp[i][j1] = min(dp[i][j1],
+                                        dp[i - 1][j2]
+                                        + j1 * b[i]);
+                    }
+
+                }
+            }
+        }
+        ll ans = INF;
+        ans = min(ans, dp[n][0]);
+        ans = min(ans, dp[n][1]);
+        ans = min(ans, dp[n][2]);
+        cout << ans << endl;
     }
-    cout << ans << endl;
+
+
     return 0;
 }
+
