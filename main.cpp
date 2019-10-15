1f289ec4596525f3193afd0cae7931acd6194126
TieWay59 <tieway59@foxmail.com>
2019-10-15 22:32:24 +0800

    + lazy update

--- main.cpp
+++ main.cpp
@@ -1,36 +1,137 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
-typedef long long ll;
+#define  _debug(x) cerr<<#x << " = " << x<<endl
 using namespace std;
 
-//
-ll exGCD(ll a, ll b, ll &x, ll &y) {
-    if (b == 0) {
-        x = 1;
-        y = 0;
-        return a;
+typedef long long ll;
+//typedef _int128 lll;
+const double eps = 1e-3;
+const int MAXN = 1e5 + 59;
+const ll MOD = 1e9 + 7;
+const int MAXM = 2e4 + 59;
+const int INF = 0x3f3f3f3f;
+const ll INFll = 0x3f3f3f3f3f3f3f;
+
+map<int, int> dsc;
+//int csd[MAXN << 1];
+
+#define lson (oo<<1)
+#define rson (oo<<1|1)
+int tr[MAXN << 3];
+
+void update(int oo, int l, int r, int pos, int val) {
+    if (l == r && r == pos) {
+        tr[oo] += val;
+        return;
     }
-    ll gcd = exGCD(b, a % b, x, y);
-    ll old_x = x;
-    x = y;
-    y = old_x - (a / b) * x;
-    return gcd;
+    int mid = (l + r) >> 1;
+    if (pos <= mid) update(lson, l, mid, pos, val);
+    else update(rson, mid + 1, r, pos, val);
+    tr[oo] = tr[lson] + tr[rson];
 }
 
+int query(int oo, int l, int r, int L, int R) {
+    if (L <= l && r <= R) {
+        return tr[oo];
+    }
+
+    int mid = (l + r) >> 1;
+    if (mid >= R)
+        return query(lson, l, mid, L, R);
+    else if (mid < L)
+        return query(rson, mid + 1, r, L, R);
+    else
+        return query(lson, l, mid, L, R)
+               + query(rson, mid + 1, r, L, R);
+}
+
+
+struct node {
+    int x, t, v, i;
+
+    bool operator<(const node &rht) const {
+        if (x == rht.x) {
+            return t < rht.t;
+        }
+        return x < rht.x;
+    }
+} a[MAXN * 3];
+
+int ans[MAXN];
+
 int main() {
-    //do this
-    ll a, b, x, y, gcd;
-    while (cin >> a >> b) {
-        gcd = exGCD(a, b, x, y);
-        if (gcd != 1) {
-            cout << "sorry\n";
-        } else {
-            if (x < 0) {
-                ll num = (abs(x) - 1) / (b / gcd) + 1;
-                x += num * (b / gcd);
-                y -= num * (a / gcd);
+    freopen("business.in","r+",stdin);
+    int kase;
+    int n, m;
+    scanf("%d", &kase);
+    while (kase--) {
+        dsc.clear();
+        int tot = 0;
+        int maxPos = 0;
+        scanf("%d%d", &n, &m);
+
+        for (int i = 1, x, q, r; i <= n; ++i) {
+            scanf("%d%d%d", &x, &q, &r);
+            dsc[q - x] = 1;
+            a[tot++] = {x, 1, q - x, i};
+            a[tot++] = {x + r, 3, q - x, i};
+        }
+
+        for (int i = 1, y, d; i <= m; ++i) {
+            scanf("%d%d", &y, &d);
+            dsc[d - y] = 1;
+            a[tot++] = {y, 2, d - y, i};
+        }
+
+        for (auto &di:dsc) {
+            di.second = ++maxPos;
+//            csd[maxPos] = di.first;
+        }
+
+        for (int j = 0; j < tot; ++j) {
+            a[j].v = dsc[a[j].v];
+        }
+        sort(a, a + tot);
+
+//        _debug(maxPos);
+        for (int i = 0; i < tot; ++i) {
+//            _debug(a[i].v);
+//            _debug(a[i].x);
+//            _debug(a[i].t);
+//            _debug(a[i].i);
+//            _debug(i);
+//            _debug("                                           ");
+            if (a[i].t == 1) {
+                update(1, 1, maxPos, a[i].v, 1);
+            } else if (a[i].t == 2) {
+                ans[a[i].i] = query(1, 1, maxPos, a[i].v, maxPos);
+            } else if (a[i].t == 3) {
+                update(1, 1, maxPos, a[i].v, -1);
             }
-            cout << x << ' ' << y << '\n';
+
+//            for (int j = 1; j <= maxPos; j++) {
+//                printf("%d%c", query(1, 1, maxPos, j, j), " \n"[j == maxPos]);
+//            }
+        }
+
+        for (int i = 1; i <= m; ++i) {
+            printf("%d%c", ans[i], " \n"[i == m]);
         }
     }
 }
+/*
+
+2
+3 3
+1 2
+2 3
+3 1
+6 6
+1 2
+2 3
+3 1
+2 4
+4 5
+5 2
+
+ */
