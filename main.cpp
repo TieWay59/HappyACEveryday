dd11290a9206f4ab1a4546cc29dd361e09bd8695
TieWay59 <tieway59@foxmail.com>
2019-11-03 13:30:00 +0800

    + lazy update

--- main.cpp
+++ main.cpp
@@ -1,44 +1,70 @@
 #include <bits/stdc++.h>
 #include <tuple>
 
-const int MAXN = 555;
+#define  debug(x)  cerr <<#x << " = "<<x<<endl
+const int MAXN = 2e5 + 59;
 using namespace std;
-int n, kase;
-string str;
+
+typedef long long ll;
+
+int n, k;
+int a[MAXN];
+int ans;
+int minVal = 10005;
+
+int tree[MAXN];
+
+void add(int x, int val) {
+    while (x <= n) {
+        tree[x] += val;
+        x += (x & -x);
+    }
+}
+
+int ask(int x) {
+    int ret = 0;
+    while (x) {
+        ret += tree[x];
+        x -= (x & -x);
+    }
+    return ret;
+}
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+//    ios::sync_with_stdio(false);
+//    cin.tie(0);
 
-    cin >> kase;
-    while (kase--) {
-        cin >> str;
-        queue<char> odd, eve;
-        for (auto si:str) {
-            if ((si - '0') & 1) {
-                odd.emplace(si);
-            } else {
-                eve.emplace(si);
-            }
-        }
-        while (!(odd.empty() || eve.empty())) {
-            if (odd.front() < eve.front()) {
-                cout << odd.front();
-                odd.pop();
-            } else {
-                cout << eve.front();
-                eve.pop();
-            }
-        }
-        while (!odd.empty()) {
-            cout << odd.front();
-            odd.pop();
-        }
-        while (!eve.empty()) {
-            cout << eve.front();
-            eve.pop();
-        }
-        cout << endl;
+//    cin >> kase;
+//    while (kase--) {
+//    }
+
+    cin >> n >> k;
+    for (int i = 1; i <= n; ++i) {
+        cin >> a[i];
+        add(i, a[i]);
+        minVal = min(minVal, a[i]);
     }
+
+    for (int i = k, delta; i <= n; i++) {
+        delta = ask(i) - ask(i - k);
+        if (delta >= 0) {
+            delta++;
+            for (int j = i, t; delta > 0 && j > i - k; j--) {
+                t = min(delta, a[j] - minVal);
+                delta -= t;
+                a[j] -= t;
+                ans += t;
+                add(j, -t);
+            }
+        }
+    }
+    cout << ans << endl;
+    for (int i = 1; i <= n; ++i) {
+        cout << a[i] << " \n"[i == n];
+    }
+
     return 0;
 }
+/*
+
+ */
