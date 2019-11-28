4ede98945ccc67c7289cc8c7c1f16a4108f3b07a
tieway59 <tieway59@foxmail.com>
2019-11-29 01:51:55 +0800

    + timer

--- main.cpp
+++ main.cpp
@@ -16,73 +16,16 @@
 
 #include <bits/stdc++.h>
 
-#ifdef DEBUG
-//#define debug(x)  cerr <<#x << " = "<<x<<endl;
-#include "libs59/debugers.h"
-
-#else
-#define endl '\n'
-#define debug(...)  59
-#endif
-
-#define STOPSYNC ios::sync_with_stdio(false);cin.tie(nullptr)
-#define MULTIKASE int Kase=0;cin>>Kase;for(int kase=1;kase<=Kase;kase++)
-typedef long long ll;
-const int MAXN = 2e5 + 59;
-const int MOD = 1e9 + 7;
-const int INF = 0x3F3F3F3F;
-const ll llINF = 0x3F3F3F3F3F3F3F3F;
 using namespace std;
 
-string to_String(int x) {
-    stringstream ss;
-    ss << x;
-    return string(ss.str());
-}
-
 int main() {
-    string fileName;
-    ifstream cdin, cdout;
-    for (int SysCase = 1; SysCase <= 3; SysCase++) {
-//        fileName = to_String(SysCase) + ".in";
-//        cdin = ifstream(fileName);
-//        fileName = to_String(SysCase) + ".out";
-//        cdout = ifstream(fileName);
 
-        int Kase;
-        string str;
-        cdin >> Kase;
-        for (int kase = 1; kase <= Kase; kase++) {
-            int ans = 0;
-            int jury_ans = 0;
-            int n;
-            cdin >> n;
-            cdin >> str;
-            cdout >> jury_ans;
-            for (const auto &si:str) {
-                if (si == '_')ans++;
-            }
-            if (jury_ans != ans) {
-                debug(str);
-                debug(ans);
-                debug(jury_ans);
-                cout << "Wrong Answer on test #" << SysCase << endl;
-                cout << "at case #" << kase << endl;
-                /*
-                 * sahsalshla
-                 * sahishoaisioa
-                 *
-                 *
-                 */
-                exit(0);
-            }
-        }
-
-        cout << "Accepted on test #" << SysCase << endl;
-        cdin.close();
-        cdout.close();
+    clock_t begin = clock();
+    int x = 0;
+    for (int i = 1; i <= 800000000; ++i) {
+        x++;
     }
-
+    printf("%.3f ms\n", (double) (clock() - begin));
     return 0;
 }
 /*
