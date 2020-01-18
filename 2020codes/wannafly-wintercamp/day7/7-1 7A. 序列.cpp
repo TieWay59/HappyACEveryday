0df583658a6275114586ad29aeecd6f0820fa4ad
tieway59 <tieway59@foxmail.com>
2020-01-18 18:08:15 +0800

    + day7

--- 2020codes/wannafly-wintercamp/day7/7-1 7A. 序列.cpp
+++ 2020codes/wannafly-wintercamp/day7/7-1 7A. 序列.cpp
@@ -7,8 +7,8 @@
   *   ╚═╝  ╚═╝ ╚═════╝╚═╝    ╚═╝     ╚══════╝╚══════╝
   *
   *  @Author: TieWay59
-  *  @Created: 2019/11/22 21:39
-  *  @Link: Accept
+  *  @Created: 2020/1/18 14:44
+  *  @Link: https://pintia.cn/problem-sets/1218331719966515200/problems/1218332009230884871
   *  @Tags:
   *
   *******************************************************/
@@ -16,6 +16,7 @@
 #include <bits/stdc++.h>
 
 #ifdef DEBUG
+
 #   include "libs59/debugers.h"
 //  #define debug(x)  cerr <<#x << " = "<<x<<endl;
 #else
@@ -26,14 +27,48 @@
 #define STOPSYNC ios::sync_with_stdio(false);cin.tie(nullptr)
 #define MULTIKASE int Kase=0;cin>>Kase;for(int kase=1;kase<=Kase;kase++)
 typedef long long ll;
-const int MAXN = 2e5 + 59;
+const int MAXN = 5e3 + 59;
 const int MOD = 1e9 + 7;
 const int INF = 0x3F3F3F3F;
 const ll llINF = 0x3F3F3F3F3F3F3F3F;
 using namespace std;
 
-void solve(int kaseId = -1) {
+bool isCoprime[MAXN][MAXN];
+ll n, m;
+ll cnt = 0;
 
+void solve(int kaseId = -1) {
+    cin >> n;
+
+    if (n == 1) {
+        cout << "0/1" << endl;
+        return;
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = i + 1; j <= n; ++j) {
+            if (__gcd(i, j) == 1) {
+                isCoprime[i][j] = true;
+                isCoprime[j][i] = true;
+                cnt += 2;
+                debug(i, j);
+                debug(j, i);
+            }
+        }
+    }
+
+    ll ans_n = cnt;
+    ll ans_d = 2 * (n - 1);
+    if (n & 1) {
+        ans_n *= (n - 1);
+        ans_d *= n;
+    }
+
+    ll d = __gcd(ans_d, ans_n);
+    ans_d /= d;
+    ans_n /= d;
+
+    cout << ans_n << '/' << ans_d << endl;
 }
 
 void solves() {
@@ -48,5 +83,15 @@ int main() {
     return 0;
 }
 /*
+    if N is even then:
+        comment: cnt * [ 1 * (n-2)! * (n/2) ] / n!
+        answer := cnt / ( 2 * n - 2 )
+
+    else then:
+        comment: cnt * [ 1 * (n-2)! * (n/2) * ((n-1)/n)] / n!
+                diff in the pairs sequence position.
+        answer := cnt * ( n - 1 ) / n * ( 2 * n - 2 )
+
+    end if;
 
  */
--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
   *   ╚═╝  ╚═╝ ╚═════╝╚═╝    ╚═╝     ╚══════╝╚══════╝
   *
   *  @Author: TieWay59
-  *  @Created: 2020/1/18 13:43
-  *  @Link: https://pintia.cn/problem-sets/1218331719966515200/problems/1218332009230884874
+  *  @Created: 2020/1/18 15:43
+  *  @Link: https://pintia.cn/problem-sets/1218331719966515200/problems/1218332009230884864
   *  @Tags:
   *
   *******************************************************/
@@ -21,7 +21,7 @@
 //  #define debug(x)  cerr <<#x << " = "<<x<<endl;
 #else
 #   define endl '\n'
-#   define debug(...)  59
+#   define debug(...)
 #endif
 
 #define STOPSYNC ios::sync_with_stdio(false);cin.tie(nullptr)
@@ -33,46 +33,50 @@ const int INF = 0x3F3F3F3F;
 const ll llINF = 0x3F3F3F3F3F3F3F3F;
 using namespace std;
 
-ll a1, a2;
-ll b1, b2;
-ll n;
+
+bool check_bits(int bits, int i) {
+    return bits & (1 << --i);
+}
+
+int insert_bits(int bits, int i) {
+    return bits | (1 << --i);
+}
+
+vector<int> ed[21];
+int n, m, q;
+int lastCnt[21];    // count how many black has generated for every i.
+bool vis[1 << 21 | 1023];
+
+int x;
 
 void solve(int kaseId = -1) {
-    cin >> a1 >> a2;
-    cin >> n;
-    ll ans = llINF;
-    for (int i = 1; i <= n; ++i) {
 
-        cin >> b1 >> b2;
-        ll bb1 = 0, bb2 = 0;
-        ll aa1 = a1, aa2 = a2;
-        ll cnt = 0;
+    cin >> n >> m >> q;
 
-        while (bb1 < b1 || bb2 < b2) {
-            ll t1 = aa1 > 0 ? ((b1 - bb1) / aa1) : INF;
-            ll t2 = aa2 > 0 ? ((b2 - bb2) / aa2) : INF;
-
-            if (t1 > t2) {
-                aa1++;
-            } else if (t1 < t2) {
-                aa2++;
-            } else {
-                if (b1 - bb1 >= b2 - bb2) {
-                    aa1++;
-                } else {
-                    aa2++;
-                }
-            }
-
-            bb1 += aa1;
-            bb2 += aa2;
-            debug(bb1, bb2);
-            cnt++;
-            if (cnt > ans)break;
-        }
-        ans = min(ans, cnt);
+    int cur = 0, tmp;
+    for (int i = 1, si; i <= n; ++i) {
+        cin >> si;
+        if (si == 1)
+            cur = insert_bits(cur, i);
     }
-    cout << ans << endl;
+
+    x++;
+
+    for (int i = 1, u, v; i <= m; ++i) {
+        cin >> u >> v;
+        ed[u].emplace_back(v);
+        ed[v].emplace_back(u);
+    }
+    while (!vis[cur]) {
+
+        for (int u = 1; u <= n; ++u) {
+            for (auto v:ed[u]) {
+
+            }
+        }
+
+    }
+
 }
 
 void solves() {
