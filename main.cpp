7864bb711bc34594159cc909277aa3c1f0100c8f
tieway59 <tieway59@foxmail.com>
2020-02-05 10:08:23 +0800

    upd

--- main.cpp
+++ main.cpp
@@ -7,9 +7,8 @@
   *   ╚═╝  ╚═╝ ╚═════╝╚═╝    ╚═╝     ╚══════╝╚══════╝
   *
   *  @Author: TieWay59
-  *  @Created: 2020/2/4 18:21
-  *  @Link: 0
-
+  *  @Created: 2020/2/4 13:36
+  *  @Link: https://vjudge.net/contest/355151?tdsourcetag=s_pctim_aiomsg#problem/H
   *  @Tags:
   *
   *******************************************************/
@@ -30,24 +29,73 @@
 typedef long long ll;
 const int MAXN = 2e5 + 59;
 const int MOD = 1e9 + 7;
-const int INF = 0x3F3F3F3F;
+const ll INF = 0x3F3F3F3F3F3F3F3F;
 const ll llINF = 0x3F3F3F3F3F3F3F3F;
 using namespace std;
 
-ll fpow(ll a, ll b, ll mod = 1000000007) {
-    //ll ret = (a % mod != 0);
-    ll ret = 1;
-    while (b) {
-        if (b & 1)ret = ret * a % mod;
-        a = a * a % mod;
-        b >>= 1;
+using cost_t = long long;   //beware of integer overflow
+using node_t = int;
+using edge_t = pair<node_t, cost_t>;
+using pqnd_t = pair<cost_t, node_t>;
+
+vector<vector<edge_t>> adj;
+vector<cost_t> tag;
+
+void dijkstra(int s, vector<cost_t> &d) {
+    int n = adj.size();
+    d.assign(n, INF);   // distance
+
+    d[s] = 0;
+    priority_queue<pqnd_t, vector<pqnd_t>, greater<pqnd_t>> q;
+    q.emplace(0, s);
+
+    node_t u, v;
+    cost_t dis, len;
+    while (!q.empty()) {
+        dis = q.top().first;
+        u = q.top().second;
+        q.pop();
+        if (dis > d[u]) // i.e. !=
+            continue;
+
+        for (auto edge : adj[u]) {
+            v = edge.first;
+            len = edge.second;
+            if (d[u] + len + tag[v] < d[v]) {
+                d[v] = d[u] + len + tag[v];
+                q.emplace(d[v], v);
+            }
+        }
     }
-    return ret;
 }
 
-
 void solve(int kaseId = -1) {
-    cout << fpow(55, 0) << endl;
+    ll n, m, k;
+    cin >> n >> m >> k;
+    adj.assign(n + 1, {});
+    tag.assign(n + 1, 0);
+
+    for (ll i = 1, u, v, w; i <= m; ++i) {
+        cin >> u >> v >> w;
+        adj[u].emplace_back(v, w);
+        adj[v].emplace_back(u, w);
+    }
+
+    for (ll i = 1; i <= n; ++i) {
+        cin >> tag[i];
+    }
+
+    for (ll j = 1, u; j <= k; ++j) {
+        cin >> u;
+        adj[0].emplace_back(u, 0);
+        adj[u].emplace_back(0, 0);
+    }
+
+    vector<cost_t> dist;
+    dijkstra(0, dist);
+    for (ll i = 1; i <= n; ++i) {
+        cout << dist[i] << endl;
+    }
 }
 
 void solves() {
